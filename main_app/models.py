d8ae75393b014783e63f7912b180f4c271a5b26d
rpeterson87 <oshorhino5053@gmail.com>
2022-10-07 13:53:27 -0400

    Add home test route and view

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+# Need to add model for Users 
+
+
+# Need to add model for Workouts 
+
+
+# Need to add model for exercises
+
+
+# Need to add a model for embedded videos
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,11 @@
+import imp
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
 # Create your views here.
+
+class Home(View):
+    def get(self, request):
+        return HttpResponse("Fitness-x Home")
+
