6d011736188fb0fa22b5d721c13ce11b241d2b4f
rpeterson87 <oshorhino5053@gmail.com>
2022-10-10 16:29:37 -0400

    Base exercise template working.

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 import imp
 from django.contrib import admin
-from .models import Workouts
+from .models import Workouts, Exercises
 
 # Register your models here.
 
 admin.site.register(Workouts)
+admin.site.register(Exercises)
